26c3cede5e0301f23ad3aa7807832b71d453d4db
devzero-deploys <72330074+devzero-deploys@users.noreply.github.com>
2020-11-01 00:42:30 -0300

    Update index.jsx

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -24,7 +24,7 @@ function Chat() {
       setCurrentRoom(rooms[0]);
     });
 
-    setTimeout(() => setLoading(false), 2000);
+    document.onload = setLoading(false);
   }, []);
 
   useEffect(_ => {
